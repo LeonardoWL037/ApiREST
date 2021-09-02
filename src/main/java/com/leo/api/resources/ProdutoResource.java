b9c670c3674abc6e0144ec1f66cfb177ef066cb5
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-09-01 23:00:13 -0300

    Adicionado o método de cadastro de produtos

--- src/main/java/com/leo/api/resources/ProdutoResource.java
+++ src/main/java/com/leo/api/resources/ProdutoResource.java
@@ -1,10 +1,11 @@
 package com.leo.api.resources;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +27,13 @@ public class ProdutoResource {
 	
 	
 	@GetMapping("/produtos/{id}")
-	public Optional<Produto> buscaProduto(Long id){
+	public Produto buscaProduto(@PathVariable(value="id") long id){
 		return produtorepository.findById(id);
 	}
 	
+	
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtorepository.save(produto);
+	}
 }
