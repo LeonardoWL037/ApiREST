4b593d7ac2ca1c8bad9f0cb8317e43874e92bd8e
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-09-01 22:33:56 -0300

    Criação de repository e resource

---
+++ src/main/java/com/leo/api/resources/ProdutoResource.java
@@ -0,0 +1,27 @@
+package com.leo.api.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leo.api.models.Produto;
+import com.leo.api.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoResource {
+
+	@Autowired
+	ProdutoRepository produtorepository;
+	
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos(){
+		return produtorepository.findAll();
+	}
+	
+	
+}
