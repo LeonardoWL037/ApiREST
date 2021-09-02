654c3cb58b5ff118bce403fc4fd40f7628fa7891
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-09-01 23:16:24 -0300

    Criação do método de atualização de produto

--- src/main/java/com/leo/api/resources/ProdutoResource.java
+++ src/main/java/com/leo/api/resources/ProdutoResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,12 @@ public class ProdutoResource {
 	@DeleteMapping("/produto")
 	public void deletaProduto(@RequestBody Produto produto) {
 		produtorepository.delete(produto);
+	}
+	
+	
+	@PutMapping("/produto")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		return produtorepository.save(produto);
 	}
 	
 }
